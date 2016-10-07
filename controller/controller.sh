d64320122aedce086b27d695d9896a79b3ee9c26
Gerard L. Petersen <gerard.petersen@gmail.com>
2016-10-07 10:31:04 +0100

    Home and away logic done - added more actions and log

--- controller/controller.sh
+++ controller/controller.sh
@@ -3,26 +3,91 @@
 myMacAddress="";
 
 function amIHome() {
-    currentlyHome=$1;
+    threshHold=200
 
-    if [ ${currentlyHome} = true ]; then
-        echo "home";
-        # When was the last time I was away
-        # If it was longer than 10 minutes ago - I have come home - call home.sh
+    isHome=$1;
+    isHome=true;
+    homeTheWholeTime=`cat ../log/log.txt | awk '/CURRENTLYHOME/{print $2}'`
+    lastRun=`cat ../log/log.txt | awk '/LASTRUN/{print $2}'`
+    currentTime=$(date +%s);
+
+    if [ $((currentTime-lastRun)) -gt ${threshHold} ]; then
+        resetLog ${currentTime}
+    fi
+
+    if [ ${isHome} = true ]; then
+        # Tell the log I am home
+        logTime ${isHome}
+        # Network says I am home - have I been here the whole time?
+        if [ ${homeTheWholeTime} -le 0 ]; then
+            # Log says I've not been at home - I have just come home
+            # run action
+            actuallyHome
+        else
+            ../actions/still-home.sh
+        fi
     else
-        echo "away";
-        # When was the last time I was home
-        # If it was longer than 10 minutes ago - I have come home - call away.sh
+        # Network says I am away - have I been away the whole time?
+        if [ ${homeTheWholeTime} -eq -1 ]; then
+            # Log says it that it ain't no snitch and it ain't telling us shit - So we don't tell the log shit neither
+            # Wait till I've been away for more than threshold to run
+            lastAway=`cat ../log/log.txt | awk '/LASTAWAY/{print $2}'`
+            if [ $((currentTime-lastAway)) -gt ${threshHold} ]; then
+                actuallyAway
+            else
+                ../actions/might-be-away.sh
+            fi
+        elif [ ${homeTheWholeTime} -eq 1 ]; then
+            # Tell the log I am away
+            logTime ${isHome}
+            # Log says I was just here a second ago - Wait till I've not been seen for longer than threshold to run
+            lastHome=`cat ../log/log.txt | awk '/LASTHOME/{print $2}'`
+            if [ $((currentTime-lastHome)) -gt ${threshHold} ]; then
+                actuallyAway
+            else
+                ../actions/might-be-away.sh
+            fi
+        else
+            ../actions/still-away.sh
+        fi
+
     fi
 }
 
-currentlyHome=false;
+
+
+function resetLog {
+    currentTime=$1;
+    echo "resetting as ${currentTime}"
+}
+
+# Logs the last time we were home, away and last time the script was run
+function logTime {
+    isHome=$1;
+    if [ ${isHome} = true ]; then
+        echo "logging currently home"
+    else
+        echo "logging currently away"
+    fi
+}
+
+
+function actuallyHome {
+    ../actions/home.sh
+}
+
+function actuallyAway {
+    ../actions/away.sh
+}
+
+
+isHome=false;
 
 while read -r networkMacAddress; do
     if [ ${networkMacAddress} = ${myMacAddress} ]; then
-        currentlyHome=true;
+        isHome=true;
     fi
 done
 
-amIHome ${currentlyHome}
+amIHome ${isHome}
 
